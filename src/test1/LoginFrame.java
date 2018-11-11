f088d91630b88f69d5a8d2ab616f00da30771b18
keelim/CP2_201503069 <kimh00335@gmail.com>
2018-11-11 17:11:49 +0900

    login update
    login update

--- src/test1/LoginFrame.java
+++ src/test1/LoginFrame.java
@@ -10,7 +10,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class LoginFrame extends JFrame {
+public class LoginFrame extends JFrame implements ActionListener {
 	private JTextField loginField;
 	private JPasswordField passwordField;
 
@@ -26,6 +26,7 @@ public class LoginFrame extends JFrame {
 
 		passwordField = new JPasswordField();
 		passwordField.setBounds(188, 84, 116, 21);
+		passwordField.addActionListener(this);
 		getContentPane().add(passwordField);
 
 		JLabel lblId = new JLabel("ID");
@@ -34,46 +35,49 @@ public class LoginFrame extends JFrame {
 
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setBounds(68, 87, 57, 15);
+		
 		getContentPane().add(lblPassword);
 
 		JButton btnNewButton = new JButton("Sign in");
 		btnNewButton.setBounds(316, 53, 85, 52);
-		btnNewButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO 자동 생성된 메소드 스텁
-				String id = loginField.getText();
-				char[] pass = passwordField.getPassword();
-				String password = new String(pass);
-
-				if (id.equals("") || password.equals("")) {
-					JOptionPane.showMessageDialog(null, "비어있습니다.");
-				} else {
-					boolean existLogin = Login.loginTest(id, password);
-
-					if (existLogin) {
-						// 로그인 성공일 경우
-						JOptionPane.showMessageDialog(null, "로그인 성공");
-						MainFrame m = new MainFrame();
-						
-						
-					} else {
-						// 로그인 실패일 경우
-						JOptionPane.showMessageDialog(null, "로그인 실패");
-						
-					}
-				}
-				
-				
-			}
-
-		});
+		btnNewButton.addActionListener(this);
 		getContentPane().add(btnNewButton);
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO 자동 생성된 메소드 스텁
+		String id = loginField.getText();
+		char[] pass = passwordField.getPassword();
+		String password = new String(pass);
 
+		if (id.equals("") || password.equals("")) {
+			JOptionPane.showMessageDialog(null, "비어있습니다.");
+		} else {
+			boolean existLogin = Login.loginTest(id, password);
+
+			if (existLogin) {
+				// 로그인 성공일 경우
+				JOptionPane.showMessageDialog(null, "로그인 성공");
+				MainFrame m = new MainFrame();
+				this.setVisible(false);
+				
+				
+			} else {
+				// 로그인 실패일 경우
+				JOptionPane.showMessageDialog(null, "로그인 실패");
+				loginField.setText(""); //로그인 실패시 --> 모든 칸 빈칸으로
+				passwordField.setText("");
+				
+			}
+		}
+		
+		
+		
+	}
+	
 	
 
 }
--- src/test1/Main.java
+++ src/test1/Main.java
@@ -1,12 +1,7 @@
 package test1;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO 자동 생성된 메소드 스텁
-		
-		LoginFrame m = new LoginFrame();
-		
+		new LoginFrame();
 	}
-
 }
