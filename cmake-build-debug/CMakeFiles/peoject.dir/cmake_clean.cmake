0234c1cfd914ca591ea29d3ab3009a4677fc1a5d
Keel_im <kimh00335@gmail.com>
2019-02-07 22:45:52 +0900

    2019 02 07 1

---
+++ cmake-build-debug/CMakeFiles/peoject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/peoject.dir/main.c.obj"
+  "peoject.pdb"
+  "peoject.exe"
+  "peoject.exe.manifest"
+  "peoject.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/peoject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
