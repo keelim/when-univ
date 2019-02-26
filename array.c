818d4f8cbd8dacb92e596b3483a625530d771ef7
Keel_im <kimh00335@gmail.com>
2019-02-26 23:52:19 +0900

    Update array.c

--- array.c
+++ array.c
@@ -1,42 +1,52 @@
-
 #include<stdio.h>
 
+void sum(int i);
+
+void alphabet();
+
+void array_change();
+
 int main(void) {
-//    int sbuway_1 = 10;
-//    int sbuway_2 = 20;
-//    int sbuway_3 = 30;
 
-    int subway[3];
-//    초기화 하지 않으면 더미 값이 들어간다.
-//    subway[0] = 10;
-//    subway[1] = 20;
-//    subway[2] = 30;
-//
-//    for (int i = 0; i < sizeof(subway)/ sizeof(int); ++i) {
-//        printf("%d\n", subway[i]);
-//    }
-
-//    int arr[10] = {0};
-//     하나라도 선언을 하면 전부 0으로 초기화가 된다.
-//    for (int i = 0; i < sizeof(arr)/ sizeof(int); ++i) {
-//        printf("%d\n", arr[i]);
-//    }
-
-//    float arr_f[5] = {0};
-//    for (int j = 0; j < sizeof(arr_f)/ sizeof(float); ++j) {
-//        printf("%f\n", arr_f);
-//        printf("안녕");
-//    }
-
-    char c_array[10]= {'c', 'o', 'd', 'i', 'n', 'g'};
-
-    for (int i = 0; i < sizeof(c_array); ++i) {
-        printf("%c\n", c_array[i]);
-    }
-
-
-
+    sum(100);
+    alphabet();
+    array_change();
 
     return 0;
 
+}
+
+void array_change() {
+    int es[10];
+    for (int i = 0; i < sizeof(es); ++i) {
+        es[i] = i;
+    }
+
+    for (int i = 0; i < sizeof(es); ++i) {
+        printf("%d ", es[i]);
+    }
+
+    for (int j = 0; j < sizeof(es); ++j) {
+        es[j] = 0;
+    }
+
+    for (int i = 0; i < sizeof(es); ++i) {
+        printf("%d ", es[i]);
+    }
+
+}
+
+void alphabet() {
+    for (int i = 97; i <= 122; ++i) {
+        printf("출력값 %c\n", (char)i);
+    }
+}
+
+void sum(int i) {
+    int a = 0;
+    for (int j = 0; j <= i; ++j) {
+        a += j;
+    }
+
+    printf("출력값: %d", a);
 }
