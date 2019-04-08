44f7769e454f84a77dd296d00ac574600002d473
Keel_im <kimh00335@gmail.com>
2019-04-08 22:47:55 +0900

    modify .gitignore

--- _DS_04_201503069_kimjaehyun.java
+++
@@ -1,6 +0,0 @@
-public class _DS_04_201503069_kimjaehyun {
-    public static void main(String[] args) {
-        AppController appController = new AppController();
-        appController.run();
-    }
-}
