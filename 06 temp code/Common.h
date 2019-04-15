23e68b1b3af297965fb93f30ef3f4930f40b0d5b
Keel_im <kimh00335@gmail.com>
2019-04-15 12:43:41 +0900

    2019 04 15

--- 06 temp code/Common.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include<stdlib.h>
-#include<stdio.h>
-typedef enum { FALSE, TRUE } Boolean;
-//동적 할당
-#define NewVector(TYPE, SIZE) (TYPE*)malloc(sizeof(TYPE)*SIZE)
-#define NewObject(TYPE) (TYPE*)malloc(sizeof(TYPE))
-//swap
-#define SWAP(TYPE, X, Y) {TYPE temp = X; X=Y; Y=temp;} 
