73eb681a63920e1ca8da18b34f0250cf56cb7215
Keel_im <kimh00335@gmail.com>
2019-04-16 22:18:50 +0900

    2019 04 16

---
+++ 06/AppView.c
@@ -0,0 +1,63 @@
+﻿#include "AppView.h"
+
+void AppView_out(char* message)
+{
+	printf("%s\n", message);
+}
+
+void AppView_out_averageScore(float anAverageScore) {
+	printf("\n평균 점수는 %.2f 입니다.\n ", anAverageScore);
+}
+
+Boolean AppView_in_doesContinueToInputNextStudent(){
+	printf("? 성적 입력하려면 'Y' , 입력을 종료하려면 다른 아무키나 치시오 \n");
+	char answer;
+	answer = getcharDirectlyFromKeyBoard();
+
+	return (answer == 'Y' || answer == 'y');
+}
+
+int AppView_in_score() {
+	int score;
+	printf("점수를 입력하시오: ");
+	scanf_s("%d", &score);
+	printf("\n");
+
+	return score;
+}
+
+char getcharDirectlyFromKeyBoard()
+{
+	char charFromKeyBoard;
+	charFromKeyBoard = _getch();
+
+	return charFromKeyBoard;
+}
+
+
+void AppView_out_numberOfStudentsAboveAverage(int aNumber) {
+	printf("평균 이상인 학생은 모두 %d 입니다. \n", aNumber);
+}
+
+void AppView_out_maxScore(int aMaxScore) {
+
+	printf("학생들의 최고점은 %d 입니다. \n", aMaxScore);
+
+}
+void AppView_out_minScore(int aMinScore) {
+
+	printf("학생들의 최저점은 %d 입니다. \n\n", aMinScore);
+
+}
+void AppView_out_gradeCountFor(char aGrade, int aCount) {
+	printf("%c 학점은 %d명 입니다. \n", aGrade, aCount);
+}
+
+void AppView_out_studentinfo(int score, char grade) {
+	printf("점수: %d 학점: %c\n", score, grade);
+}
+
+
+
+
+
---
+++ 06/Ban.c
@@ -0,0 +1,242 @@
+﻿#include"Common.h"
+#include "Ban.h"
+
+#define DEFAULT_CAPACITY 100
+struct _Ban {
+	int _capacity;
+	int _size;
+	int* _elements; //동적 배열
+};
+
+int Ban_capacity(Ban* _this) //capacity getter
+{
+	return _this->_capacity;
+}
+
+int Ban_size(Ban* _this) //size getter
+{
+	return _this->_size;
+}
+
+Ban* Ban_new() //Ban constructor
+{
+	Ban* _this = NewObject(Ban);
+	_this->_capacity = DEFAULT_CAPACITY;
+	_this->_size = 0;
+	_this->_elements = NewVector(int, _this->_capacity);
+	return _this;
+}
+
+Ban* Ban_newWidthCapacity(int givenCapacity) //Ban parameter constructor
+{
+	Ban* _this = NewObject(Ban);
+	_this->_capacity = givenCapacity;
+	_this->_size = 0;
+	_this->_elements = NewVector(int, _this->_capacity);
+	return _this;
+}
+
+Boolean Ban_isEmpty(Ban* _this) //비어 있는지를 확인
+{
+	return (_this->_size == 0);
+}
+
+Boolean Ban_isFull(Ban * _this) //꽉 차 있는지를 확인
+{
+	return (_this->_size >= _this->_capacity);
+}
+
+Boolean Ban_scoreIsValid(int aScore) //점수의 유효성을 확인
+{
+	return (aScore >= 0 && aScore <= 100);
+}
+
+void Ban_delete(Ban * _this) //Ban 소멸
+{
+	free(_this->_elements);
+	free(_this);
+}
+
+Boolean Ban_add(Ban * _this, int aScore) //Ban의 점수를 추가
+{
+	if (Ban_isFull(_this)) {
+		return FALSE;
+	}
+	else {
+		_this->_elements[_this->_size] = aScore;
+		_this->_size++;
+		return TRUE;
+	}
+}
+
+int Ban_elementAt(Ban * _this, int anOrder) //특정 인데스에서의 엘리먼트 추가
+{
+	if (anOrder >= _this->_size) {
+		return -1;
+	}
+	else {
+		return (_this->_elements[anOrder]);
+	}
+}
+
+char Ban_scoreToGrade(int aScore) //해당 점수의 맞는 등급을 리턴
+{
+	if (aScore >= 90) {
+		return 'A';
+	}
+	else if (aScore >= 80) {
+		return 'B';
+	}
+	else if (aScore >= 70) {
+		return 'C';
+	}
+	else if (aScore >= 60) {
+		return 'D';
+	}
+	else {
+		return 'F';
+	}
+}
+
+void Ban_sortStudentsByScore(Ban * _this) //점수 순으로 정렬을 한다. 
+{
+	int size = _this->_size; //quick sort 실행
+
+	if (size >= 2) {
+		int minPosition = 0;
+		for (int i = 1; i < size; i++) {
+			if (_this->_elements[i] < _this->_elements[size - 1]) {
+				minPosition = i;
+			}
+		}
+
+		SWAP(int, _this->_elements[minPosition], _this->_elements[size - 1]);
+
+		Ban_quickSortRecursively(_this, 0, size - 2);
+	}
+}
+
+void Ban_quickSortRecursively(Ban * _this, int left, int right) //quicksort 실행
+{
+	if (left < right) {
+		int mid = Ban_partition(_this, left, right); //파티션 함수 실행
+		Ban_quickSortRecursively(_this, left, mid - 1); //mid 왼쪽
+		Ban_quickSortRecursively(_this, mid + 1, right); //mid 오른쪽
+
+
+	}
+}
+
+int Ban_partition(Ban * _this, int left, int right) //quick Sort에 필요한 파티션을 선택
+{
+	int pivot = left;
+	int pivotScore = _this->_elements[pivot]; //피봇을 정의한다. 
+
+	right++;
+	do
+	{
+		do { left++; } while (_this->_elements[left] > pivotScore); //피봇을 기준으로 왼쪽과 
+		do { right--; } while (_this->_elements[right] < pivotScore); //오른쪽의 포인트를 움직인다. 
+		if (left < right) {
+			SWAP(int, _this->_elements[left], _this->_elements[right]);
+		}
+
+	} while (left < right);
+	SWAP(int, _this->_elements[pivot], _this->_elements[right]); //왼쪽 위치와 오른쪽에서의 위치를 교환
+
+	return right;
+}
+
+float Ban_averageScore(Ban * _this)
+{
+	float sumOfScores = (float)Ban_sumOfScoresRecursively(_this, 0, _this->_size - 1);
+	float average = sumOfScores / (float)_this->_size;
+
+	return average;
+}
+
+int Ban_maxScore(Ban * _this) 
+{
+	return Ban_maxOfScoresRecursively(_this, 0, _this->_size - 1);
+}
+
+int Ban_minScore(Ban * _this)
+{
+	return Ban_minOfScoresRecurively(_this, 0, _this->_size - 1);
+}
+
+int Ban_numberOfStudentsAboveAverage(Ban * _this)
+{
+	float average = Ban_averageScore(_this);
+	int numberOfStudentsAboveAverage = 0;
+
+	for (int i = 0; i < _this->_size; i++)
+	{
+		if ((float)_this->_elements[i] >= average) {
+			numberOfStudentsAboveAverage++;
+		}
+	}
+	return numberOfStudentsAboveAverage;
+}
+
+GradeCounter* Ban_countGrades(Ban * _this)
+{
+	char currentGrade;
+	GradeCounter* gradeCounter = GradeCounter_new();
+
+	for (int i = 0; i < _this->_size; i++) {
+		currentGrade = Ban_scoreToGrade(_this->_elements[i]);
+		GradeCounter_count(gradeCounter, currentGrade);
+	}
+	return gradeCounter;
+}
+
+int Ban_sumOfScoresRecursively(Ban * _this, int left, int right) { //재귀적으로 학생들의  총 점수를 얻는다. 
+	if (left > right) {
+		return 0;
+	}
+	else {
+		return (_this->_elements[left] + Ban_sumOfScoresRecursively(_this, left + 1, right));
+	}
+}
+
+int Ban_maxOfScoresRecursively(Ban * _this, int left, int right) { //재귀적으로 반을 나누어서 최댓값을 택한다. 
+	int maxOfLeftPart;
+	int maxOfRightPart;
+	int mid;
+
+	if (left == right) {
+		return _this->_elements[left];
+	}
+	else {
+		mid = (right + left) / 2;
+		maxOfLeftPart = Ban_maxOfScoresRecursively(_this, left, mid); //재귀적 call 반에서의 최댓값
+		maxOfRightPart = Ban_maxOfScoresRecursively(_this, mid + 1, right);
+
+		if (maxOfLeftPart >= maxOfRightPart) { //더 큰 값을 선택
+			return maxOfLeftPart;
+		}
+		else {
+			return maxOfRightPart;
+		}
+	}
+}
+
+int Ban_minOfScoresRecurively(Ban * _this, int left, int right) { //재귀적으로 Ban에서 하나씩 줄여 나가는 생각으로 최솟값을 찾는다. 
+
+	int minPart;
+	if (left == right) {
+		return _this->_elements[left];
+	}
+	else {
+		minPart = Ban_minOfScoresRecurively(_this, left + 1, right); //다시 call
+		if (_this->_elements >= minPart) {
+			return minPart; //적은 파트 리턴
+		}
+		else {
+			return _this->_elements[left]; //적은 값을 리턴
+		}
+	}
+}
+
+
