2f3dfac9ed7a78eb441dea606efd36523447db49
keelim <kimh00335@gmail.com>
2019-05-08 22:51:00 +0900

    2019 05 08

---
+++ 09-1/AppController.java
@@ -0,0 +1,194 @@
+public class AppController {
+    private static final int QUEUE_CAPACITY = 10;
+    // 비공개 변수들
+    private Queue<Character> _queue; //만들 거야
+    private int _inputChars; // 입력된 문자의 개수
+    private int _addedChars; // 삽입된 문자의 개수
+    private int _ignoredChars; // 무시된 문자의 개수
+
+    public Queue<Character> queue() {
+        return _queue;
+    }
+
+    public void setQueue(Queue<Character> _queue) {
+        this._queue = _queue;
+    }
+
+    public int inputChars() {
+        return _inputChars;
+    }
+
+    public void setInputChars(int _inputChars) {
+        this._inputChars = _inputChars;
+    }
+
+    public int addedChars() {
+        return _addedChars;
+    }
+
+    public void setAddedChars(int _addedChars) {
+        this._addedChars = _addedChars;
+    }
+
+    public int ignoredChars() {
+        return _ignoredChars;
+    }
+
+    public void setIgnoredChars(int _ignoredChars) {
+        this._ignoredChars = _ignoredChars;
+    }
+
+    public AppController() {
+        this.setQueue(new CircularArrayQueue<Character>(AppController.QUEUE_CAPACITY)); // todd
+        this.setInputChars(0);
+        this.setAddedChars(0);
+        this.setIgnoredChars(0);
+    }
+
+    // 비공개 함수
+// 횟수 계산
+    private void countInputChar() {
+        this.setInputChars(this.inputChars() + 1);
+    }
+
+    private void countIgnoredChar() {
+        this.setIgnoredChars(this.ignoredChars() + 1);
+    }
+
+    private void countAddedChar() {
+        this.setAddedChars(this.addedChars() + 1);
+    }
+
+    // 큐 수행 관련
+    private void addToQueue(char aCharForAdd) { //todo
+//큐가 가득 찬 경우
+//"[EnQ.Empty] 큐가 꽉 차서 더 이상 넣을 수가 없습니다." 를 출력
+//정상적으로 enQueue가 되었을 경우
+//삽입된 원소를 출력한다.
+//this.countAdded() 를 호출하여 삽입된 원소의 숫자를 증가시킨다.
+//삽입하는 원소의 자료형이 기본 자료형인 “char” 가 아니라, class “Character” 이다.
+    }
+
+    private void removeOne() {
+        if (this.queue().isEmpty()) {
+            AppView.outputLine("[DeQ.Empty] 큐에 삭제할 원소가 없습니다. ");
+        } else {
+            Character removedChar = this.queue().deQueue(); //Character 객체
+            if (removedChar == null) {
+                AppView.outputLine("(오류) 큐에서 삭제하는 동안 오류가 발생하였습니다. ");
+
+            } else {
+                AppView.outputLine("[Deq] 삭제된 원소는" + removedChar + "입니다.");
+            }
+        }
+    }
+
+    private void removeN(int numberOfCharsToBeRemoved) {
+//삭제할 개수가 0 개이면 다음과 같이 출력:
+//"[DeQs] 삭제할 원소의 개수가 0 개입니다."
+//“deQueue()” 를 삭제하는 횟수만큼 실행하여, 매번 다음과 같이 출력한다: (삭제된 원소가 'X' 라면)
+//"[DeQs] 삭제된 원소는 'X' 입니다."
+//삭제 오류가 발생하면 다음과 같이 출력:
+//"(오류) 큐에서 삭제하는 동안에 오류가 발생하였습니다."
+//삭제를 반복하는 동안에, 큐가 비게 되어 더 이상 삭제가 불가능하게 되면 다음과 같은 메시지를 출력한다
+//"[DeQs.Empty] 큐에 더 이상 삭제할 원소가 없습니다."
+
+    }
+
+    private void quitQueueProcessing() {
+        this.showAllFromFront();
+        removeN(this.queue().size());
+    }
+
+    // 출력 관련
+    private void showAllFromFront() {
+        // 큐의 모든 원소를 Front 부터 Rear 까지 출력한다.
+        // Iterator 를 사용한다.
+        AppView.output("[Queue] <Front> ");
+        Iterator<Character> queueIterator = this.queue().iterator();
+        while (queueIterator.hasNext()) {
+            Character element = queueIterator.next();
+            AppView.output(element.toString() + " ");
+        }
+        AppView.outputLine("<Rear>");
+
+    }
+
+    private void showAllFromRear() {
+        // 큐의 모든 원소를 Rear 부터 Front 까지 출력한다.
+        // elementAt() 을 사용한다.
+        AppView.output("[Queue] <Rear> ");
+        for (int order = this.queue().size() - 1; order >= 0; order--) {
+            AppView.output(this.queue().elementAt(order).toString() + " ");
+        }
+        AppView.outputLine("<Front>");
+
+    }
+
+    private void showFrontElement() { //todo
+//Queue 객체의 front() 을 이용하여 Front 원소를 출력
+//큐가 비어 있으면, front 원소 대신 비어 있다는 메시지 출력
+
+    }
+
+    private void showRearElement() { //todo
+//Queue 객체의 rear() 를 이용하여 Rear 원소를 출력
+//큐가 비어 있으면, Rear 원소 대신 비어 있다는 메시지 출력
+
+    }
+
+    private void showQueueSize() { //todo
+//Queue 객체의 size() 를 이용하여 원소의 개수를 출력
+
+    }
+
+    private void showStatistics() {
+        AppView.outputLine("");
+        AppView.outputLine("<큐 사용 통계>");
+        AppView.outputLine(" - 입력된 문자는" + inputChar() + " 개 입니다. ");
+        AppView.outputLine(" - 정상 처리된 문자는" + (this.inputChar() - this.ignoredChars()) + " 개 입니다.");
+        AppView.outputLine(" - 무시된 문자는" + this.ignoredChars() + " 개 입니다.");
+        AppView.outputLine(" - 삽입된 문자는" + this.addedChars() + " 개 입니다.");
+    }
+
+    // 입력 관련
+    private char inputChar() {
+        return '0';
+    }
+
+    public void run() {
+        AppView.outputLine("<<<큐 기능 확인 프로그램을 시작합니다 >>>");
+        AppView.outputLine("");
+        char input = this.inputChar();
+        while (input == '!') {
+            this.countInputChar();
+            if (Character.isAlphabetic(input)) {
+                this.addToQueue(Character.valueOf(input));
+            } else if (Character.isDigit(input)) {
+                this.removeN(Character.getNumericValue(input));
+            } else if (input == '-') {
+                this.removeOne();
+            } else if (input == '#') {
+                this.showQueueSize();
+            } else if (input == '/') {
+                this.showAllFromFront();
+            } else if (input == '\\') {
+                this.showAllFromRear();
+            } else if (input == '<') {
+                this.showFrontElement();
+            } else if (input == '>') {
+                this.showRearElement();
+            } else {
+                AppView.outputLine("[Ignore] 의미 없는 문자가 입력되었습니다.");
+                this.countIgnoredChar();
+            }
+            input = this.inputChar();
+        }
+        this.quitQueueProcessing();
+        this.showStatistics();
+        AppView.outputLine("");
+        AppView.outputLine("<<<큐 기능 확인 프로그램을 종료합니다 >>>");
+
+
+    }
+}
---
+++ 09-1/CircularArrayQueue.java
@@ -0,0 +1,179 @@
+public class CircularArrayQueue<E> implements Queue<E> {
+
+    // Constants
+    private static final int DEFAULT_CAPACITY = 100;
+    // Instance Variables
+    private int _maxLength; // capacity+1
+
+    private int _frontPosition;
+
+    private int _rearPosition;
+    private E[] _elements;
+
+    public int maxLength() {
+        return _maxLength;
+    }
+
+    public void setMaxLength(int _maxLength) {
+        this._maxLength = _maxLength;
+    }
+
+    public int frontPosition() {
+        return _frontPosition;
+    }
+
+    public void setFrontPosition(int _frontPosition) {
+        this._frontPosition = _frontPosition;
+    }
+
+    public int rearPosition() {
+        return _rearPosition;
+    }
+
+    public void setRearPosition(int _rearPosition) {
+        this._rearPosition = _rearPosition;
+    }
+
+    public E[] elements() {
+        return _elements;
+    }
+
+    public void setElements(E[] _elements) {
+        this._elements = _elements;
+    }
+
+    public int capacity() {
+        return (this.maxLength() - 1); //환형 큐의 특징
+    }
+
+    @SuppressWarnings("unchecked")
+    public CircularArrayQueue(int givenCapacity) {
+        this.setMaxLength(givenCapacity + 1);
+        this.setElements((E[]) new Object[this.maxLength()]);
+    }
+
+    public CircularArrayQueue() {
+        this(CircularArrayQueue.DEFAULT_CAPACITY);
+    }
+
+    @Override
+    public int size() { //환형 큐에서의 size method
+        if (this.rearPosition() >= this.frontPosition()) {
+            return (this.rearPosition() - this.frontPosition());
+        } else {
+            return (this.rearPosition() + this.maxLength() - this.frontPosition());
+        }
+    }
+
+    @Override
+    public boolean isFull() {
+//        다음 삽입될 위치가 this.frontPosition() 과 같으면 true 를, 아니면 false 를 반환.
+        int nextRearPosition = (this._rearPosition + 1) % this._maxLength;
+        return (nextRearPosition == this._frontPosition);
+    }
+
+    @Override
+    public boolean isEmpty() {//front rear position이 같을 때
+        return (this._frontPosition == this._rearPosition);
+
+    }
+
+    @Override
+    public E front() {
+//        비어 있는 경우 null 을 반환
+//        비어 있지 않은 경우 큐의 맨 앞에 있는 원소를 반환
+        E frontElement = null;
+        if (!this.isEmpty()) {
+            frontElement = this._elements[this._frontPosition + 1];
+        }
+        return frontElement;
+
+    }
+
+    @Override
+    public E rear() {
+//        비어 있는 경우 null 을 반환
+//        비어 있지 않은 경우 큐의 맨 뒤에 있는 원소를 반환
+        E rearElement = null;
+        if (!this.isEmpty()) {
+            rearElement = this._elements[this._rearPosition];
+        }
+        return rearElement;
+
+    }
+
+    @Override
+    public boolean enQueue(E anElement) {
+        if (this.isFull()) {
+            return false;
+        } else {
+            this.setRearPosition((this._rearPosition + 1) % this._maxLength);
+            this.elements()[this.rearPosition()] = anElement;
+            return true;
+        }
+    }
+
+    @Override
+    public E deQueue() { //todo 애는 새로이 이해를 하는 것이 필요
+        E frontElement = null;
+        if (!this.isEmpty()) {
+            this._frontPosition = (this._frontPosition + 1) % this._maxLength;
+            frontElement = this._elements[this._frontPosition];
+            this._elements[this._frontPosition] = null;
+        }
+        return frontElement;
+
+    }
+
+    @Override
+    public void clear() {
+        this._frontPosition = 0;
+        this._rearPosition = 0;
+        for (int i = 0; i < this.maxLength(); i++) {
+            this._elements[i] = null;
+        }
+
+    }
+
+    @Override
+    public E elementAt(int anOrder) { //todo 이 코드가 맞는지 확인
+        return this.elements()[((this.frontPosition() + 1 + anOrder) % this.maxLength())];
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new CircularArrayQueueIterator();
+    }
+
+    private class CircularArrayQueueIterator implements Iterator<E> {
+
+        private int _nextOrder;
+
+        public void setNextOrder(int _nextOrder) {
+            this._nextOrder = _nextOrder;
+        }
+
+        public int nextOrder() {
+            return _nextOrder;
+        }
+
+        public CircularArrayQueueIterator() {
+            this.setNextOrder(0);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (this.nextOrder() < CircularArrayQueue.this.size());
+        }
+
+        @Override
+        public E next() {
+            E nextElement = null;
+            if (this.hasNext()) {
+                nextElement = CircularArrayQueue.this.elements()[this.nextOrder()];
+                this.setNextOrder(this.nextOrder() + 1);
+            }
+            return nextElement;
+        }
+    }
+}
