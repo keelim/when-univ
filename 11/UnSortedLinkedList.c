38dd6d829a6bf48dcf48c66afdc716aec7c2aeec
Keel_im <kimh00335@gmail.com>
2019-05-13 16:27:04 +0900

    Update UnSortedLinkedList.c

--- 11/UnSortedLinkedList.c
+++ 11/UnSortedLinkedList.c
@@ -1,18 +1,33 @@
 #pragma once
+
 #include "UnSortedLinkedList.h"
+#include "Node.h"
 
 //todo 소스 채울 것
+struct _UnsortedLinkedList {
+    int size;
+    Node *_head;
+};
+
 
 UnsortedLinkedList *UnsortedLinkedList_new() {
-    return NULL;
+    UnsortedLinkedList *_this = NewObject(UnsortedLinkedList);
+    _this->_head = Node_new();
+    Node_setElement(_this->_head, NULL);
+    _this->size = 0;
 }
 
 void UnsortedLinkedList_delete(UnsortedLinkedList *_this) {
-
+    Node_delete(_this->_head);
+    free(_this);
 }
 
 Boolean UnsortedLinkedList_isEmpty(UnsortedLinkedList *_this) {
-    return FALSE;
+    Boolean flag = FALSE;
+    if (_this->size == 0) {
+        flag = TRUE;
+    }
+    return flag;
 }
 
 Boolean UnsortedLinkedList_isFull(UnsortedLinkedList *_this) {
@@ -20,7 +35,12 @@ Boolean UnsortedLinkedList_isFull(UnsortedLinkedList *_this) {
 }
 
 Boolean UnsortedLinkedList_add(UnsortedLinkedList *_this, Element anElement) {
-    return FALSE;
+    Node *addNode = Node_new();
+    Node_setElement(addNode, anElement);
+    Node_setNext(addNode, _this->_head);
+    
+    _this->size++;
+    return TRUE;
 }
 
 Element UnsortedLinkedList_min(UnsortedLinkedList *_this) {
