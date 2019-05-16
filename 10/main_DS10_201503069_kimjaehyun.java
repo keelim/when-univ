ad2afd1e7230a1c20d851372450863a24ce0d1f5
keelim <kimh00335@gmail.com>
2019-05-16 11:16:47 +0900

    2019 05 16

--- 10/main_DS10_201503069_kimjaehyun.java
+++
@@ -1,6 +0,0 @@
-public class main_DS10_201503069_kimjaehyun {
-    public static void main(String[] args) {
-        AppController appController= new AppController() ;
-        appController.run () ;
-    }
-}
