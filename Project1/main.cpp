287d3e5263c1848704ce11be000e18ee5ce072d1
keelim <kimh00335@gmail.com>
2019-10-31 23:41:07 +0900

    2019 10 31

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,35 +1,17 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 int main () {
 	int testCase;
 	cin >> testCase;
-
-	for (auto i=1; i < testCase; i++) {
-		int answer;
-		vector<int> list;
-		for (auto i=0; i < 5; i++) {
-			int num;
-			cin >> num;
-			list.push_back (num);
+	for (auto i=0; i < testCase; i++) {
+		int answer=0;
+		for (auto j=0; j < 10; j++) {
+			int temp;
+			cin >> temp;
+			answer+=temp;
 		}
-
-		int a, b, c, d, e,f,g;
-		a=list[0];
-		b=list[1];
-		c=list[2];
-		d=list[3];
-		e=list[4];
-		f=a * e;
-
-		if (e <= c) {
-			g=b;
-		}
-		else {
-			g=b +(e - c) * d;
-		}
-		answer=(f > g) ? g : f;
-		cout << "#" << i << " " << answer << endl;
+		answer/=10;
+		cout << "#" << i << " " << answer << "\n";
 	}
 }
