cbf437d2d4deddf460d51b00a4d889873ce5f981
keelim <kimh00335@gmail.com>
2019-11-19 00:54:58 +0900

    2019 11 19

--- src/db/DbCall.java
+++ src/db/DbCall.java
@@ -4,14 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-public class practice {
+public final class DbCall { // static 으로 작성을 해야 하나?
     private static Connection con = null;
     private static PreparedStatement pstmt = null;
     private static ResultSet rs = null;
     private static String sql = null;
-    private DBConnectionMgr pool;
+    private static DBConnectionMgr pool;
 
-    public practice() {
+    public DbCall() {
         pool = DBConnectionMgr.getInstance();
     }
 
@@ -43,4 +43,32 @@ public class practice {
 //        }
 //        return flag; //결과값 리터
 //    }
+
+    public static void call(){
+        boolean flag = false;
+        String getPass;
+
+        try {
+            con = pool.getConnection();
+            sql = "select PW from user where ID=?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, PW);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                // 패스워드를 읽어온다.
+                getPass = rs.getString("PW");
+                // 데이터베이스에서 읽어온 문자열과 사용자가 입력한 비밀번호가 같을 경우에는
+                if (getPass.equals(PW)) {
+//					System.out.println("받아온 비밀번호 :" + getPass); --> 비밀 번호를 확인을 하는 핸들러
+                    flag = true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 자원반납
+            pool.freeConnection(con, pstmt, rs);
+        }
+        return flag; //결과값 리터
+    }
 }
