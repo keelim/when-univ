503485c807390102289f0fcfc71043056b8e09ac
h0033 <kimh00335@gmail.com>
2020-03-21 17:16:41 +0900

    Initial commit

---
+++ src/1/JP01.java
@@ -0,0 +1,7 @@
+
+public class JP01 {
+    public static void main(String[] args) { // 1
+        System.out.println("나도 이제 Java 프로그램을 할 수 있다."); //2
+    }
+}
+
---
+++ src/1/JP01_2.java
@@ -0,0 +1,9 @@
+public class JP01_2 {
+    public static void main(String[] args) {
+        int x;  //1
+        x = 20; //2
+        System.out.println("나의 나이는"); //3
+        System.out.print(x); //4
+        System.out.println("살 입니다."); //5
+    }
+}
