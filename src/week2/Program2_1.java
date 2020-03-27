a7cb54e3d531ab49bfa59c80fc7111d1d9321382
h0033 <kimh00335@gmail.com>
2020-03-27 13:28:18 +0900

    2020 03 27

--- src/week2/Program2_1.java
+++ src/week2/Program2_1.java
@@ -8,25 +8,25 @@ public class Program2_1 {
         // 서로 다른 실근
         a = 1.0; b = 0.0; c = -4.0;
 
-        a=3.0; b=0.0; c=-27.0;
-
+//        a=3.0; b=0.0; c=-27.0;
+//
         // 중근
         a=1.0; b=2.0; c=1.0;
 
-        a=1.0; b=2.0; c=4.0;
-        
-        // 허근
-        a=1.0; b=1.0; c=1.0;
-
-        a=2.0; b=2.0; c=2.0;
+//        a=1.0; b=2.0; c=4.0;
+//
+//        // 허근
+//        a=1.0; b=1.0; c=1.0;
+//
+//        a=2.0; b=2.0; c=2.0;
         System.out.println("a= " + a + "b= " + b + "c= " + c);
         determinant = b * b - 4.0 * a * c;
         if(determinant>0){
-            x1 = (-b+Math.sqrt(determinant)/(2.0*a));
-            x2 = (-b-Math.sqrt(determinant)/(2.0*a));
+            x1 = ((-b+Math.sqrt(determinant))/(2.0*a));
+            x2 = ((-b-Math.sqrt(determinant))/(2.0*a));
             System.out.println("The solution is either"+x1+" or "+x2);
         } else if (determinant ==0){
-            x1 = (-b+Math.sqrt(determinant)/(2.0*a));
+            x1 = ((-b+Math.sqrt(determinant))/(2.0*a));
             System.out.println("The solution is only" +x1);
         } else {
             System.out.println("The solution is NaN");
